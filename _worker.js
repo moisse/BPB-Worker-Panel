aaf5aa2fc4a84a91e2756990de3aa30497668828
moisse <50827308+moisse@users.noreply.github.com>
2024-09-09 00:25:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'aa763668-d69e-4238-b2ac-4d12c1dc4c2d';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
